7fece0ea33ce32190d761d8dae62ca133c3c9eb6
Janny Au <janny.au@outlook.com>
2021-06-27 17:32:02 +0800

    refind release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,14 @@
 #/bin/bash
-echo "Cleaning the working space..."
+echo "[1/4] Cleaning the working space..."
 git clean -xfd
-echo "Packing the release code..."
+echo
+echo "[2/4] Packing the release code..."
 python3 setup.py sdist bdist_wheel
-echo "Listing the packages to be released..."
+echo
+echo "[3/4] Listing the files to be released..."
 ls dist
-echo "Releasing the code..."
+echo
+echo "[4/4] Releasing the code..."
 read -n 1 -s -r -p "Press any key to continue...."
 twine upload dist/*
 
